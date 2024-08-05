9e8e5c69ea9971e7ea4163325710337ec1e561ee
vartika1910 <Vartika.Yadav@Triconinfotech.com>
2024-08-05 17:27:12 +0530

    updated main.yml

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/fastapi_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/fastapi_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
