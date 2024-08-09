de12e7573315faf94fe4ea385ea18d4d5e642b17
Vartika Yadav <65497059+vartika1910@users.noreply.github.com>
2024-08-09 15:22:48 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@host.docker.internal:3306/fastapi_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/fastapi_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
